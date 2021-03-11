31130f78073a294dd7facdd1e648b2178793fbe2
Elizangela Calegari <elizangelac@ciandt.com>
2021-03-11 14:56:09 -0300

    Calcula idade

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//using Shared;
 
 namespace CoreTest
 {
@@ -6,7 +7,22 @@ namespace CoreTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oi, está pronto para a diversão? (S/N)");
+
+            var resultado = Console.ReadLine();
+
+            if(resultado == "S")
+            {
+                Console.WriteLine("Qual a sua data de nascimento? (dd/MM/yyyy)");
+                DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+
+                //var idade = Pessoa.CalcularIdade(dataNascimento);
+
+                Console.WriteLine($"Sua idade é: {idade}");
+            }
+            else{
+                Console.WriteLine("Que pena =/");
+            }
         }
     }
 }
