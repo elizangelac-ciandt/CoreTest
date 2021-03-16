e756cb98e05b6d00d43b2db8660af0d9f96b212e
Elizangela Calegari <elizangelac@ciandt.com>
2021-03-16 09:37:40 -0300

    Fixes

--- Program.cs
+++ Program.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using Shared;
+using SharedIngest;
 
 namespace CoreTest
 {
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			Console.WriteLine("Oi, está pronto para a diversão? (S/N)");
+		{            
+			DateTime birthDate = new DateTime(1988, 1, 25);
 
-			Console.WriteLine("Qual a sua data de nascimento? (dd/MM/yyyy)");
-            
-			DateTime dataNascimento = new DateTime(1988, 1, 25);
+			var years = Person.CalculateAge(birthDate);
 
-			var idade = Pessoa.CalcularIdade(dataNascimento);
-
-			Console.WriteLine($"Sua idade é: {idade}");
+			Console.WriteLine($"Your age is: {years}");
 		}
 	}
 }
